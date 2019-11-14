180e5b2fa35f9b0c067c0d481b764fc34c7ced2a
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-13 18:55:27 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+// import ExchangedCurrency from './components/CurrencyConverter';
 import './App.css';
+
+import CurrencyHOC from './components/CurrencyConverter';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
-    );
+      <>
+        <h2>Higher Order Component</h2>
+        <CurrencyHOC />
+      </>
+    )
   }
 }
 
